21aaf94c78c17f7d9023c322459c2a1f29ec50c5
Michael Anderson <nomicflux@gmail.com>
2015-11-30 15:43:23 -0600

    Added page handler

--- src/Api.hs
+++ src/Api.hs
@@ -7,30 +7,40 @@ import Control.Monad (liftM)
 import Control.Monad.Reader (ReaderT, runReaderT, lift)
 import Control.Monad.Trans.Either (EitherT, left)
 import Network.Wai (Application)
+import Network.HTTP.Types
 import Database.Persist.Postgresql (selectList, Entity(..), (==.), fromSqlKey, toSqlKey, insert)
 import Data.Int (Int64)
 import Servant
+import Servant.Docs
 
 import Config (Config(..))
 import Models
 import Api.User
 import Api.Lesson
 import Api.App
+import Api.Page
 
-type FullAPI = "users" :> UserAPI
-               :<|> "lessons" :> LessonAPI
+type HandledAPI = "users" :> UserAPI
+                  :<|> "lessons" :> LessonAPI
+                  :<|> "page" :> PageAPI
 
-fullAPI :: Proxy FullAPI
-fullAPI = Proxy
+handledAPI :: Proxy HandledAPI
+handledAPI = Proxy
+
+type WholeAPI = HandledAPI :<|> Raw
+
+wholeAPI :: Proxy WholeAPI
+wholeAPI = Proxy
 
 readerToEither :: Config -> AppM :~> EitherT ServantErr IO
 readerToEither cfg = Nat $ \x -> runReaderT x cfg
 
-readerServer :: Config -> ServerT FullAPI AppM -> Server FullAPI
-readerServer cfg server = enter (readerToEither cfg) server
+readerServer :: Config -> ServerT HandledAPI AppM -> Server WholeAPI
+readerServer cfg server = (enter (readerToEither cfg) server)
+                          :<|> serveDirectory "static"
 
 app :: Config -> Application
-app cfg = serve fullAPI (readerServer cfg fullServer)
+app cfg = serve wholeAPI (readerServer cfg fullServer)
 
-fullServer :: ServerT FullAPI AppM
-fullServer = userServer :<|> lessonServer
+fullServer :: ServerT HandledAPI AppM
+fullServer = userServer :<|> lessonServer :<|> pageServer
---
+++ src/Api/Page.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Api.Page where
+
+import Control.Monad.Reader (ReaderT, runReaderT, lift)
+import Control.Monad.Trans.Either (EitherT, left)
+import Database.Persist.Postgresql (selectList, Entity(..), (==.), fromSqlKey, toSqlKey, insert)
+import Data.Int (Int64)
+import Lucid
+import Servant
+import Servant.HTML.Lucid
+
+import Models
+import Api.App
+
+data Page = Page
+            { number :: Int64
+            , lesson :: Lesson
+            }
+
+instance ToHtml Page where
+  toHtml (Page num lesson) =
+    html_ $ do
+      head_ $ do
+        title_ (toHtml $ "Flux")
+        (script_ [src_ (toText $ "/elm/" ++ (lessonJsfile lesson) ++ ".js")] "")
+        link_ [href_ $ toText "/css/bootstrap.css", type_ $ toText "text/css", rel_ $ toText "stylesheet"]
+        link_ [href_ $ toText "/css/lessons.css", type_ $ toText "text/css", rel_ $ toText "stylesheet"]
+      body_ $ do
+        div_ $ do
+          h1_ (toHtml $ lessonTitle lesson)
+          h2_ (toHtml $ lessonKeyphrase lesson)
+        with div_ [id_ (toText "elmApp"), class_ (toText "jumbotron")] (toHtml "")
+        (script_ [] "var elmApp = document.getElementById(\"elmApp\"); Elm.embed(Elm.Lesson1, elmApp);")
+  toHtmlRaw = toHtml
+
+pageAPI :: Proxy PageAPI
+pageAPI = Proxy
+
+type PageAPI =
+  Capture "pageNum" Int64 :> Get '[HTML] Page
+--  :<|> ReqBody '[JSON] Lesson :> Post '[JSON] Int64
+  -- Eventually, post pages too to create
+
+pageServer :: ServerT PageAPI AppM
+pageServer = getPage
+
+getPage :: Int64 -> AppM Page
+getPage lessonId = do
+  lesson <- runDb $ selectList [LessonId ==. (toSqlKey lessonId)] []
+  case lesson of
+    [] -> lift $ left err404
+    ((Entity _ x):xs) -> return $ Page lessonId x
